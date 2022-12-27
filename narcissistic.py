82688f9fbb0011877fbad1e197994053ce487fba
unknown <seanj.parrott@gmail.com>
2022-12-26 18:45:27 -0800

    pep8

--- narcissistic.py
+++ narcissistic.py
@@ -1,5 +1,8 @@
 def narcissistic(value):
-    import math        
-    return (sum([(int(i) ** (int(math.log10(value) + 1))) for i in str(value)])) == value
+    import math
+    return (
+        sum([(int(i) ** (int(math.log10(value) + 1))) for i in str(value)])
+    ) == value
+
 
 print(narcissistic(153))
