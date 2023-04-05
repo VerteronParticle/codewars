0da3a9b33d7851753505d1239e8084ff532e9e74
unknown <seanj.parrott@gmail.com>
2023-04-04 22:06:23 -0700

    pep8, docstrings

--- tower_builder.py
+++ tower_builder.py
@@ -1,4 +1,14 @@
+"""Build a tower of n_floors floors."""
+
+
 def tower_builder(n_floors):
+    """
+    Build a tower n_floors tall.
+
+    :param n_floors: integer representation
+                     of total number of floors.
+    :return export:  container of floor strings.
+    """
     i = 1
     m = n_floors - 1
     export = []
@@ -6,7 +16,7 @@ def tower_builder(n_floors):
         export.append(
             f"{(lambda x: ' ' * x)(m)}" +
             f"{(lambda x: '*' * (x + i))(d)}" +
-            f"{(lambda x: ' ' * x)(m)}" 
+            f"{(lambda x: ' ' * x)(m)}"
         )
         i += 1
         m -= 1
