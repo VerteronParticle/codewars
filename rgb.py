00fe4024b894c098d27f431fa2c4e89771167245
unknown <seanj.parrott@gmail.com>
2023-04-04 20:12:58 -0700

    rgb

---
+++ rgb.py
@@ -0,0 +1,14 @@
+def rgb(r, g, b):
+    def valid(num):
+
+        return 255 if num > 255 else (
+            0 if num < 0 else num
+        )
+
+    def valid_hex(num):
+
+        return hex(num)[2:] if num > 10 else (
+            f"0{hex(num)[2:]}" if num < 10 else "00"
+        )
+    output = list(map(valid_hex, map(valid, [r, g, b])))
+    return ''.join([n for n in output]).upper()
