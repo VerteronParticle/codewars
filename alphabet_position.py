f1c2e1cad935031b46169a9b61b087a4eda0c395
unknown <seanj.parrott@gmail.com>
2023-04-04 20:32:49 -0700

    docstrings

--- alphabet_position.py
+++ alphabet_position.py
@@ -1,5 +1,8 @@
 """Return numeric value of all characters in text."""
+
+
 def alphabet_position(text):
+    """Return numeric value, account for offset."""
     return ' '.join(
         str(ord(c) - 96) for c in text.lower() if c.isalpha()
     )
